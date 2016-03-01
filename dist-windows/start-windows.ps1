eac6416d8fbe4f9a661fe1a08bf83b8c91e16455
Mark DeLillo <mdelillo@pivotal.io>
2016-03-01 14:53:04 -0500

    Select first available subnet and corresponding domain
    [#112812829]
    
    Signed-off-by: Keaty Gross <kgross@pivotal.io>

--- dist-windows/start-windows.ps1
+++ dist-windows/start-windows.ps1
@@ -19,11 +19,13 @@ Function Compare-Semver([string]$Version, [string]$MinVersion) {
 }
 
 if ($Env:VBOX_INSTALL_PATH) {
-  $virtualBoxVersion = & ("$Env:VBOX_INSTALL_PATH" + "VBoxManage.exe") --version
+  $vBoxManagePath = "$Env:VBOX_INSTALL_PATH" + "VBoxManage.exe"
+  $virtualBoxVersion = & $vBoxManagePath --version
 }
 
 if ($Env:VBOX_MSI_INSTALL_PATH) {
-  $virtualBoxVersion = & ("$Env:VBOX_MSI_INSTALL_PATH" + "VBoxManage.exe") --version
+  $vBoxManagePath = "$Env:VBOX_MSI_INSTALL_PATH" + "VBoxManage.exe"
+  $virtualBoxVersion = & $vBoxManagePath --version
 }
 
 $virtualBoxSemver = Select-String -pattern [\d]+.[\d]+.[\d]+ -InputObject $virtualBoxVersion | % { $_.Matches } | % { $_.Value }
@@ -49,6 +51,24 @@ if ($visualRedistributableInstalled -ne 1) {
   Write-Host "`nThe Visual C++ 2010 Redistributable Package (x86) is required to run Vagrant on Windows systems. Please follow this link to download and install:`n
   https://www.microsoft.com/en-us/download/details.aspx?id=5555`n"
   exit
+}
+
+$inets = Ipconfig
+$vBoxInets = & $vBoxManagePath list hostonlyifs
+
+for ($i = 1; $i -le 9; $i++) {
+  $inet = "192.168.$i$i.1"
+  $Env:PCFDEV_IP = "192.168.$i$i.11"
+
+  if (!(Select-String -quiet -pattern $inet -InputObject $inets) -or ((Select-String -quiet -pattern $inet -InputObject $vBoxInets) -and !(Test-Connection -quiet -count 1 $Env:PCFDEV_IP))) {
+    $Env:PCFDEV_DOMAIN = "local$i.pcfdev.io"
+    break
+  }
+}
+
+Trap {
+  Remove-Item Env:\PCFDEV_IP
+  Remove-Item Env:\PCFDEV_DOMAIN
 }
 
 $vagrantDir = Split-Path $MyInvocation.MyCommand.Path -Parent
