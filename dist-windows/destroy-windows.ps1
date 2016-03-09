a424e18541039bf2d7488171d45f945ec028dae1
Emily Casey <ecasey@pivotal.io>
2016-03-09 12:15:06 -0500

    Move windows dist scripts to dist/
    [#115340411]
    
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- dist-windows/destroy-windows.ps1
+++
@@ -1,4 +0,0 @@
-$vagrantDir = Split-Path $MyInvocation.MyCommand.Path -Parent
-pushd "$vagrantDir" >$null
-  vagrant destroy -f
-popd >$null
--- dist-windows/stop-windows.ps1
+++
@@ -1,4 +0,0 @@
-$vagrantDir = Split-Path $MyInvocation.MyCommand.Path -Parent
-pushd "$vagrantDir" >$null
-  vagrant halt
-popd >$null
